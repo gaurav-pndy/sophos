0f4b2f80c9f3bf6ae473349ed6177d38cafbe0ba
gaurav-pndy <gauravpndy00@gmail.com>
2025-11-24 21:03:00 +0530

    updated languages

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -1,18 +1,33 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+
 import enTranslation from "../locales/en.json";
 import ruTranslation from "../locales/ru.json";
 
-i18n.use(initReactI18next).init({
-  resources: {
-    en: { translation: enTranslation },
-    ru: { translation: ruTranslation },
-  },
-  lng: "ru", // Default language
-  fallbackLng: "en",
-  interpolation: {
-    escapeValue: false,
-  },
-});
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: enTranslation },
+      ru: { translation: ruTranslation },
+    },
+
+    fallbackLng: "ru", // Default if nothing is detected
+
+    detection: {
+      order: ["querystring", "localStorage", "navigator"],
+      lookupQuerystring: "lng",
+
+      // VERY IMPORTANT → this makes "ru" the default
+      caches: ["localStorage"],
+      checkWhitelist: true,
+    },
+
+    interpolation: {
+      escapeValue: false,
+    },
+  });
 
 export default i18n;
