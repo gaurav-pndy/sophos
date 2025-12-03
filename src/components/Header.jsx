ef575f20c4e9ace88c2f043c09a4d990c76f98e6
gaurav-pndy <gauravpndy00@gmail.com>
2025-12-03 23:05:28 +0530

    updated text sizes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1201,7 +1201,7 @@ const Header = ({ city, setCity, setShowPopup, setShowUserAccount }) => {
                 </button>
               </div>
               {/* 
-              <div className="flex flex-colmb-6">
+              <div className="flex flex-colm-6">
                 <label htmlFor="city" className="text-xs mb-1">
                   {t("header.selectCity")}
                 </label>
